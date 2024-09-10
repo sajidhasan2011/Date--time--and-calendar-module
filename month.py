3088680d940993717c0c00747f5853042aeb7333
Sajid <afrojaakter874@gmail.com>
2024-09-10 23:27:19 +0600

    abcd

---
+++ month.py
@@ -0,0 +1,6 @@
+import calendar
+
+year = int(input("Enter The Year That you want to know :"))
+month = int(input("Enter The month The month of the Year You want to know :"))
+
+print(calendar.month(year,month))
